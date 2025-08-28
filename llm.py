cf6a24fc91b39bc37d009326b8e048026a0a8010
sanjanasowmya-wq <s.sanjana.tuvvara@gmail.com>
2025-08-28 00:57:33 +0000

    add llm.py script

--- llm.py
+++ llm.py
@@ -1,2 +1,23 @@
 
-print("Hello from LLM script!")
+print("pip install langchain langchain-google-genai langchain-tavily tavily-python
+       pip install langgraph
+       import os
+       from tavily import TavilyClient
+
+
+       os.environ["TAVILY_API_KEY"] = "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"  
+
+      def search_and_print(query):
+          api_key = os.environ["TAVILY_API_KEY"]  
+          tavily_client = TavilyClient(api_key=api_key)
+
+          response = tavily_client.search(query)
+
+         for result in response["results"]:
+             print(f"Title: {result['title']}")
+             print(f"Content: {result['content']}")
+             print(f"URL: {result['url']}")
+             print("-" * 50)
+
+
+    search_and_print("what is open ai")")
